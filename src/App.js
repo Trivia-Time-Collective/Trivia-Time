7bcb971e83c707d9d20b98873ea1c9e3b955b77f
Gavyn Holt <gavynholt@gmail.com>
2021-07-30 20:22:25 -0400

    created a mock firebase user database

--- src/App.js
+++ src/App.js
@@ -1,14 +1,40 @@
 import './App.scss';
+import firebase from './firebaseConfig.js';
 
 import Home from './Home';
 import Lobby from './Lobby';
 import Game from './Game';
 
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 function App() {
   const [listOfUsers, setListOfUsers] = useState([]);
+  const [fBListOfUsers, setFBListOfUsers] = useState([]);
+  console.log(fBListOfUsers);
+
+  // testing out firebase. not funnctional yet
+  useEffect(() => {
+    const roomCode = 'ABC123';
+    const dbRef = firebase.database().ref(`sessions/${roomCode}`);
+
+    dbRef.on('value', (snapshot) => {
+      const myData = snapshot.val();
+      console.log(myData);
+      const newArray = [];
+      for (let objKey in myData) {
+        const userObj = {
+          key: objKey,
+          username: myData[objKey].username,
+          points: myData[objKey].points,
+        };
+        newArray.push(userObj);
+      }
+      setFBListOfUsers(newArray);
+    });
+  }, []);
+  //------------- end of test
+
   return (
     <Router>
       <header className='wrapper'>
