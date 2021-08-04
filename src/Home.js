493e748d69761290655cd3bbc58581db548c6f47
Munira <muniraqazi98@gmail.com>
2021-08-04 19:41:16 -0400

    added a instructions modal to Home.js

--- src/Home.js
+++ src/Home.js
@@ -1,15 +1,31 @@
 import { Link } from 'react-router-dom';
+import swal from 'sweetalert';
 
 const Home = () => {
+
+  const showInstructions = () => {
+    swal ({
+      icon: 'info',
+      text:`
+      - Click Play Now
+      - Enter a user name and choose an avatar
+      - Choose a category, difficulty and question type
+      - Answer all questions within the time limit 
+      - When your turn is complete pass the phone to /n
+       the next player  on your team
+      - Impress all your friends with your trivia knowledge!`
+    })
+  }
+
   return (
     <main className="wrapper">
       <div className="btnContainer">
         <Link className="button" to="/lobby">
           Play Now
         </Link>
-        <Link className="button" to="/instructions">
+        <button className="button" onClick={showInstructions}>
           Instructions
-        </Link>
+        </button>
         <Link className="button" to="/leaderboard">
           Leaderboard
         </Link>
