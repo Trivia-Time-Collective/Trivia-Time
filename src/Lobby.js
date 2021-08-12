20e85a590214f840cdf0e256b44414a04ba9bfb1
Gavyn Holt <gavynholt@gmail.com>
2021-08-12 18:11:21 -0400

    feature to reset username in multiplayer lobby

--- src/Lobby.js
+++ src/Lobby.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Link, useHistory } from 'react-router-dom';
 import swal from 'sweetalert';
 import firebase from './firebaseConfig.js';
@@ -7,20 +7,43 @@ import axios from 'axios';
 import User from './User.js';
 
 const Lobby = ({ listOfUsers, roomCode, setQuestionsArray, triviaCategory, setTriviaCategory, triviaDifficulty, setTriviaDifficulty, isOnlineMultiplayer }) => {
+  const [showAddUserField, setShowAddUserField] = useState(true);
   const [usernameInput, setUsernameInput] = useState('');
-
+  const [userRef, setUserRef] = useState('');
   const [triviaQuestionType, setTriviaQuestionType] = useState('multiple');
   const history = useHistory();
 
-  const userRef = firebase.database().ref(`sessions/${roomCode}`);
+  const roomRef = firebase.database().ref(`sessions/${roomCode}`);
 
   const removeUser = (userKeyToDelete) => {
-    userRef.child(userKeyToDelete).remove();
+    roomRef.child(userKeyToDelete).remove();
   };
 
-  // Check if username is already taken and input is not empty, then push user to Firebase
-  const addUser = (e) => {
+  const resetUserField = () => {
+    setUsernameInput('');
+    setShowAddUserField(true);
+  };
+
+  const addOrUpdateUser = (e) => {
     e.preventDefault();
+    if (isOnlineMultiplayer) {
+      updateUser();
+    } else {
+      addUser(e);
+    }
+  };
+
+  // For Online Multiplayer. Function to update user by their given Firebase userRef.
+  const updateUser = () => {
+    userRef.update({
+      username: usernameInput,
+    });
+    setShowAddUserField(false);
+  };
+
+  // For Local Hot Seat. Function to add multiple users in one browser.
+  // Check if username is already taken and input is not empty, then push user to Firebase
+  const addUser = () => {
     const userToAdd = usernameInput.trim();
     let usernameTaken = false;
     listOfUsers.forEach((userObj) => {
@@ -34,7 +57,7 @@ const Lobby = ({ listOfUsers, roomCode, setQuestionsArray, triviaCategory, setTr
       newUser.username = userToAdd;
       newUser.points = 0;
       newUser.avatarImg = `https://robohash.org/${newUser.username}`;
-      userRef.push(newUser);
+      roomRef.push(newUser);
     } else if (usernameTaken) {
       swal('Whoops!', 'That username is taken.', 'warning');
     }
@@ -100,31 +123,55 @@ const Lobby = ({ listOfUsers, roomCode, setQuestionsArray, triviaCategory, setTr
     }
   };
 
+  // Online Multiplayer: On page load, Set a user object on Firebase with a default username.
+  useEffect(() => {
+    // Create A template user for Online Multiplayer version
+    if (isOnlineMultiplayer) {
+      const newUser = {};
+      newUser.username = 'TriviaBuff';
+      newUser.points = 0;
+      newUser.avatarImg = `https://robohash.org/TriviaBuff`;
+      newUser.isTurnComplete = false;
+      const newUserRef = firebase.database().ref(`sessions/${roomCode}`).push(newUser);
+      setUserRef(newUserRef);
+    }
+  }, [isOnlineMultiplayer, roomCode]);
+
   return (
     <main className="wrapper lobbyContainer">
       <Link to="/" className="quit button">
         Home
       </Link>
       {isOnlineMultiplayer ? <h2>Room #{roomCode}</h2> : <h2>Local Hot Seat</h2>}
-      <form className="addUserForm" onSubmit={addUser}>
-        <div className="formBox">
-          <label className="sr-only" htmlFor="username">
-            Enter a username:
-          </label>
-          <input
-            type="text"
-            id="username"
-            placeholder="Enter username"
-            maxLength={12}
-            required
-            value={usernameInput}
-            onChange={(e) => setUsernameInput(e.target.value)}
-          />
+      {showAddUserField ? (
+        <form className="addUserForm" onSubmit={addOrUpdateUser}>
+          <div className="formBox">
+            <label className="sr-only" htmlFor="username">
+              Enter a username:
+            </label>
+            <input
+              type="text"
+              id="username"
+              placeholder="Enter username"
+              maxLength={12}
+              required
+              value={usernameInput}
+              onChange={(e) => setUsernameInput(e.target.value)}
+            />
+          </div>
+          <button type="submit" className="turquoiseButton">
+            Add User
+          </button>
+        </form>
+      ) : (
+        <div className="addUserForm">
+          {/* Retrieve username by filtering from listOfUsers */}
+          <h3>Welcome, {listOfUsers.filter((user) => user.key === userRef.key)[0].username}</h3>
+          <button className="turquoiseButton" onClick={resetUserField}>
+            Reset
+          </button>
         </div>
-        <button type="submit" className="turqoiseButton">
-          Add User
-        </button>
-      </form>
+      )}
       <div className="contestantsContainer">
         <ul className="userProfileList">
           {listOfUsers.map((userObj, index) => {
